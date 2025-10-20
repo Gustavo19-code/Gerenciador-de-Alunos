d3a2a4a91dc00037601219a9342156dcc4bfd216
Gustavo19-code <gusta23316@gmail.com>
2025-10-20 16:46:41 -0300

    adicao de classe

--- Main/src/historicoNotas.java
+++ Main/src/historicoNotas.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class historicoNotas {
     private final Map<Integer,Map<String,Double>> matriculas;
@@ -24,6 +25,55 @@ public class historicoNotas {
         }
          return new ArrayList<>(disciplinas.entrySet());
     }
+
+    public void remvoerMatricula(int IDestudante, String codigoDisciplina){
+       Map<String,Double> disciplinas= matriculas.get(IDestudante);
+
+       if(disciplinas!=null){
+          disciplinas.remove(codigoDisciplina);
+        }
+        if(disciplinas.isEmpty()){
+            matriculas.remove(IDestudante);
+        }
+
+    }
+
+    public double mediaDaDisciplina(String codigoDisciplina) {
+        
+        List<Double> notasDisciplina = matriculas.values().stream()
+                .map(disciplinasEstudante -> disciplinasEstudante.get(codigoDisciplina))
+                .filter(nota -> nota != null)
+                .collect(Collectors.toList());
+
+        if (notasDisciplina.isEmpty()) {
+            return 0.0;
+        }
+
+        // Calcula a soma e a média das notas
+        double somaNotas = notasDisciplina.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        return somaNotas / notasDisciplina.size();
+    }
+
+    public double mediaEstudante(int IDestudante){
+        Map<String,Double> disciplinas= matriculas.get(IDestudante);
+
+        if(disciplinas==null|| disciplinas.isEmpty()){
+            return 0.0;
+        }
+        
+        double somaNotas = disciplinas.values().stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+                return somaNotas/disciplinas.size();
+    }
+
+    
+
+
 
 
 }
