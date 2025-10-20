fe5e8f1ed4b27436fa078a2f58483b65728597e2
Gustavo19-code <gusta23316@gmail.com>
2025-10-20 17:16:37 -0300

    finalizando historico de notas

--- Main/src/historicoNotas.java
+++ Main/src/historicoNotas.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class historicoNotas {
@@ -70,10 +72,36 @@ public class historicoNotas {
 
                 return somaNotas/disciplinas.size();
     }
+     
+    public Optional<Double> obterNota(int IDestudante, String codigoDisciplina){
+        Map<String,Double> disciplinas = matriculas.get(IDestudante);
 
-    
+        if(disciplinas==null){
+            return Optional.empty();
+        }
+        return Optional.ofNullable(disciplinas.get(codigoDisciplina));
 
+    }
 
+     public List<Integer> topNEstudantesPorMedia(int N) {
+        if (N <= 0) {
+            return new ArrayList<>();
+        }
+
+        // 1. Calcula a média para cada estudante
+        Map<Integer, Double> mediasPorEstudante = matriculas.keySet().stream().collect(Collectors.toMap(id -> id,this.mediaDoEstudante ));
+
+        // 2. Ordena os IDs dos estudantes por suas médias em ordem decrescente
+        return mediasPorEstudante.entrySet().stream()
+                
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+              
+                .limit(N)
+                
+                .map(Map.Entry::getKey)
+               
+                .collect(Collectors.toList());
+    }
 
 
 }
